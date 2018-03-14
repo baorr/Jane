b17636fc6987f0c2d04148647ff40fad01277e41
baobao <baoranran@wanda.cn>
2018-03-14 17:02:10 +0800

    add hack demo

--- app/controller/ui.js
+++ app/controller/ui.js
@@ -10,6 +10,23 @@ module.exports = app => {
       await ctx.render('/ui/index');
     }
 
+    async vueView() {
+      let {ctx} = this;
+      //生成视图  
+      await ctx.render('/vue/index');
+    }
+
+    async reactView(){
+      let {ctx} = this;
+      //生成视图  
+      await ctx.render('/react/index');
+    }
+
+    async hackView(){
+      let {ctx} = this;
+      //生成视图  
+      await ctx.render('/hack/index');
+    }
   }
       
   return UiController;
--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ module.exports = app => {
 
 	app.get('/', 'ui.indexView');
 	app.get('/ui', 'ui.indexView');
+	app.get('/v', 'ui.vueView');
+	app.get('/r', 'ui.reactView');
+	app.get('/h', 'ui.hackView')
 	app.get('/musical', 'musical.indexView');
 	app.get('/live', 'musical.liveView');
 	
